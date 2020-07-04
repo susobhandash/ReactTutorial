06506eb9e0fdd6327dd73f4532794828e50b404e
Susobhan Dash <susobhan.dash@gmail.com>
2020-07-05 00:14:10 +0530

    Changed CSS

--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function MovieCard({movie}){
     return (
-         <div className="card"  >
-            <img className="card--image"
-                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                alt={movie.title + ' poster'}
+         <div className="card neo-shadow-lvl">
+            <div class="card--image">
+                <img src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                    alt={movie.title + ' poster'}
                 />
+            </div>
             <div className="card--content">
                 <h3 className="card--title">{movie.title}</h3>
                 <p><small>RELEASE DATE: {movie.release_date}</small></p>
